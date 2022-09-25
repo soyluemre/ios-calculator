79312433fc25048b41460527f312a61c9b0cd9b4
soyluemre <emresoyludev@gmail.com>
2022-09-25 19:48:22 +0300

    finish calculator

---
+++ app.js
@@ -0,0 +1,71 @@
+const currentArea = document.getElementById("currentArea");
+const previousArea = document.getElementById("previousArea");
+
+document.getElementById("table").onclick = (e) => {
+  if (e.target.className === "number") {
+    writingCurrentArea(e.target.innerText);
+  } else if (e.target.className === "operationButtons") {
+    selectOperation(e.target.innerText);
+  } else if (e.target.className === "selector") {
+    deleteCurrentArea(e.target.innerText);
+  } else if (e.target.id === "result") {
+    result();
+  }
+};
+
+const writingCurrentArea = (e) => {
+  if (e === "." && currentArea.innerText.includes(".")) return;
+  if (e === "-" && currentArea.innerText.includes("-")) return;
+  currentArea.innerText += e;
+};
+
+const deleteCurrentArea = (e) => {
+  if (e === "AC") {
+    currentArea.innerText = "";
+    previousArea.innerText = "";
+  } else if (e === "DEL") {
+    currentArea.innerText = currentArea.innerText.slice(0, -1);
+  }
+};
+const selectOperation = (e) => {
+  if (currentArea.innerText === "") return;
+  if (previousArea.innerText !== "") {
+    calculate();
+  }
+  previousArea.innerText = currentArea.innerText + e;
+  currentArea.innerText = "";
+};
+
+const calculate = () => {
+  const currentValue = parseFloat(currentArea.innerText);
+  const previousValue = parseFloat(previousArea.innerText.slice(0, -1));
+  let calcValue;
+  if (previousArea.innerText.includes("+")) {
+    calcValue = currentValue + previousValue;
+    previousArea.innerText = "";
+    currentArea.innerText = calcValue;
+  } else if (previousArea.innerText.includes("-")) {
+    calcValue = currentValue - previousValue;
+    previousArea.innerText = "";
+    currentArea.innerText = calcValue;
+  } else if (previousArea.innerText.includes("x")) {
+    calcValue = currentValue * previousValue;
+    previousArea.innerText = "";
+    currentArea.innerText = calcValue;
+  } else if (previousArea.innerText.includes("/")) {
+    calcValue = previousValue / currentValue;
+    previousArea.innerText = "";
+    currentArea.innerText = calcValue;
+  }
+};
+
+const result = () => {
+  console.log(previousArea.innerText);
+  if (currentArea.innerText == "" && previousArea.innerText !== "") {
+    currentArea.innerText = previousArea.innerText.slice(0, -1);
+    previousArea.innerText = "";
+  } else {
+    calculate();
+  }
+  console.log(currentArea.innerText);
+};
